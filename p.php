786fbd917c051f29351c2a684077f6a3d7eac70a
Joker <joker@MacBook-Pro-Joker-2.local>
2014-12-23 18:12:23 +0200

    Adding markmonitor to tests

--- p.php
+++ p.php
@@ -1,17 +1,51 @@
 <?php
 
-class StackTest extends PHPUnit_Framework_TestCase {
+include('src/whois.main.php');
 
-    public function testPushAndPop() {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
+class tests extends PHPUnit_Framework_TestCase {
 
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack) - 1]);
-        $this->assertEquals(1, count($stack));
+    public function testsEmpty() {
+        $tests = $this->getDataSet();
+        $this->assertNotEmpty($tests);
+        return $tests;
+    }
 
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(3, count($stack));
+    /**
+     * @depends testsEmpty
+     */
+    public function testsIterator($tests) {
+        $tbl = $tests->getTable('registrars');
+        for ($i = 0; $i < $tbl->getRowCount(); $i++) {
+            $test = $tbl->getRow($i);
+            $result = $this->getWhois($test['regrinfo.domain.name']);
+            foreach ($test as $name => $value) {
+
+                $this->assertEquals($value, $this->getValueByPath($result, $name));
+            }
+        }
+    }
+    
+    protected function getValueByPath($array, $path) {
+        $current = $array;
+        foreach(explode(".", $path) as $element) { // registrar.whois.servers.1.server
+            if(!isset($current[$element])) {
+                echo "???".$array;
+                echo "-- $element";
+                return false;
+            }
+            $current = $current[$element];
+        }
+        echo "++".$current;
+        return $current;
+    }
+
+    protected function getWhois($domain) {
+        $whois = new Whois();
+        return $whois->Lookup($domain);
+    }
+
+    protected function getDataSet() {
+        return new PHPUnit_Extensions_Database_DataSet_YamlDataSet("test.yml");
     }
 
 }
